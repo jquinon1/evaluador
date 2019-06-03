183fd45910a98102247d25f9c0b73c148a58c51c
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2019-06-02 20:39:35 -0500

    Registry with files

--- src/registry.cpp
+++ src/registry.cpp
@@ -5,11 +5,13 @@
 #include "helper.h"
 #include <unistd.h>
 #include "initializer.h"
+#include <fstream>
 
 using namespace std;
 // Define allowed options
 const int registry_number_parameters = 1;
 const char *registry_parameters[] = {"-n"};
+const char *repo_name = "evaluador";
 
 void interactive_registry(const char* shm_name){
   // Getting shm
@@ -71,9 +73,81 @@ void interactive_registry(const char* shm_name){
   }
   close(sm);
 }
+// Taken from stackoverflow
+bool file_exists(const std::string& name) {
+  struct stat buffer;
+  return (stat (name.c_str(), &buffer) == 0);
+}
+// https://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exist-using-standard-c-c11-c
 
-void file_registry(const char* sh_name, bool default_shn, char* parameters[]){
-  std::cout << "not implemented yet" << '\n';
+void file_registry(const char* shm_name, bool default_shn, char* parameters[],int parameters_length){
+  string temp_name = "/" + string(shm_name);
+  int sm = shm_open(temp_name.c_str(), O_RDWR, 0660);
+  if( sm < 0){
+    cerr << "Error opening shared memory: [" << errno << "] "<< strerror(errno) <<endl;
+    exit(EXIT_FAILURE);
+  }
+  void *mapped;
+  if((mapped = mmap(NULL,sizeof(struct exam),PROT_READ | PROT_WRITE, MAP_SHARED,sm,0)) == MAP_FAILED){
+    cerr << "Error mapping shared memory: [" << errno << "] " << strerror(errno) << endl;
+    exit(EXIT_FAILURE);
+  }
+  struct Resources *shResources = (struct Resources *) mapped;
+  // Parsing files
+  int start = (default_shn) ? 2 : 4;
+  for (int i = start; i < parameters_length; i++) {
+    if( file_exists(string(parameters[i]) ) ) {
+      // Get the directory
+      char *current_location = get_current_dir_name();
+      string outputfile = string(current_location).substr(0,string(current_location).find(repo_name)+strlen(repo_name)) + "/examples/" + string(parameters[i]).substr(0,string(parameters[i]).rfind('.')) + ".spl";
+      ifstream infile(parameters[i]);
+      ofstream outfile(outputfile);
+      string input;
+      char delimiter = ' ';
+      string metadata[3];
+      while (getline(infile, input)){
+        std::cout << input << '\n';
+        for (int i = 0; i < 3; i++) {
+          string data = input.substr(0,input.find(delimiter)).c_str();
+          input = input.substr(input.find(delimiter)+1);
+          metadata[i] = data;
+        }
+        int inbox = atoi(metadata[0].c_str());
+        char sample_type = (char)metadata[1][0];
+        int sample_quantity = atoi(metadata[2].c_str());
+        std::cout << inbox << sample_type << sample_quantity << '\n';
+        // Opening semaphores
+        string input_empty_name = string(shm_name) + "_inbox_" + to_string(inbox) + "_empty";
+        string input_full_name = string(shm_name) + "_inbox_" + to_string(inbox) + "_full";
+        string input_mutex_name = string(shm_name) + "_inbox_" + to_string(inbox) + "_mutex";
+        if ((shResources->shInput.Inboxes[inbox].empty = sem_open(input_empty_name.c_str(), 0)) == SEM_FAILED){
+          exit(EXIT_FAILURE);
+        }
+        if ((shResources->shInput.Inboxes[inbox].full = sem_open(input_full_name.c_str(), 0)) == SEM_FAILED){
+          exit(EXIT_FAILURE);
+        }
+        if ((shResources->shInput.Inboxes[inbox].mutex = sem_open(input_mutex_name.c_str(), 0)) == SEM_FAILED){
+          exit(EXIT_FAILURE);
+        }
+        // Create exam
+        sem_wait(shResources->shInput.Inboxes[inbox].empty);
+        sem_wait(shResources->shInput.Inboxes[inbox].mutex);
+        int position = shResources->shInput.Inboxes[inbox].in;
+        shResources->shInput.Inboxes[inbox].exams[position].sample = sample_type;
+        shResources->shInput.Inboxes[inbox].exams[position].quantity = sample_quantity;
+        shResources->shInput.Inboxes[inbox].in = (shResources->shInput.Inboxes[inbox].in + 1) % shResources->shInput.Inboxes[inbox].maximun;
+        shResources->shInput.Inboxes[inbox].current++;
+        sem_post(shResources->shInput.Inboxes[inbox].mutex);
+        sem_post(shResources->shInput.Inboxes[inbox].full);
+
+        string sample_id = string(shm_name)+"_IN_"+to_string(inbox)+"_"+to_string(position);
+        outfile << sample_id << endl;
+      }
+      outfile.close();
+      infile.close();
+    }
+  }
+  close(sm);
 }
 
 void registry(int params_length,char *params[]) {
